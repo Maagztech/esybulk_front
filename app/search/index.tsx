2ce5e7e48b2065aa3900e6133eae0e1622096c70
Asutosh Pradhan <pasutosh15@gmail.com>
2024-11-20 14:38:37 +0530

    feat: conditionally display 'Add A product' button for distributors

--- app/search/index.tsx
+++ app/search/index.tsx
@@ -1,5 +1,6 @@
 import BuySellButton from "@/components/mobileview/global/BuySellButton";
 import AddProductModal from "@/components/mobileview/global/ProductAddModal";
+import { useAuth } from "@/context/authContext";
 import { useDistributor } from "@/context/distributorContext";
 import { Ionicons } from "@expo/vector-icons";
 import React, { useState } from "react";
@@ -8,6 +9,7 @@ import { Pressable, ScrollView, Text } from "react-native";
 const search = () => {
   const { searchproducts }: any = useDistributor();
   const [visible, setVisible] = useState(false);
+  const { userInfo }: any = useAuth();
   return (
     <ScrollView>
       {searchproducts?.map((product: any) => (
@@ -21,20 +23,22 @@ const search = () => {
           >
             No products found change your search.
           </Text>
-          <Pressable
-            style={{
-              flexDirection: "row",
-              gap: 8,
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            onPress={() => setVisible(true)}
-          >
-            <Ionicons name="add" size={24} color="#966440" />
-            <Text style={{ color: "#966440", fontWeight: "bold" }}>
-              Add A product
-            </Text>
-          </Pressable>
+          {userInfo?.role === "distributor" && (
+            <Pressable
+              style={{
+                flexDirection: "row",
+                gap: 8,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              onPress={() => setVisible(true)}
+            >
+              <Ionicons name="add" size={24} color="#966440" />
+              <Text style={{ color: "#966440", fontWeight: "bold" }}>
+                Add A product
+              </Text>
+            </Pressable>
+          )}
         </>
       )}
     </ScrollView>
