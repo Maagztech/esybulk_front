6174e29c15b8e7988de02880bc73b574136a246f
Maagztech <pasutosh15@gmail.com>
2025-02-27 04:18:47 +0530

    Refactor push notification registration: improve error handling and alert user for permission requirements

--- utils/registerForPushNotificationsAsync.ts
+++ utils/registerForPushNotificationsAsync.ts
@@ -1,7 +1,7 @@
+import { Alert, Platform } from "react-native";
 import Constants from "expo-constants";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
-import { Platform } from "react-native";
 
 export async function registerForPushNotificationsAsync() {
   if (Platform.OS === "android") {
@@ -13,37 +13,43 @@ export async function registerForPushNotificationsAsync() {
     });
   }
 
-  if (Device.isDevice) {
-    const { status: existingStatus } =
-      await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== "granted") {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== "granted") {
-      throw new Error(
-        "Permission not granted to get push token for push notification!"
-      );
-    }
-    const projectId =
-      Constants?.expoConfig?.extra?.eas?.projectId ??
-      Constants?.easConfig?.projectId;
-    if (!projectId) {
-      throw new Error("Project ID not found");
-    }
-    try {
-      const pushTokenString = (
-        await Notifications.getExpoPushTokenAsync({
-          projectId,
-        })
-      ).data;
-      console.log(pushTokenString);
-      return pushTokenString;
-    } catch (e: unknown) {
-      throw new Error(`${e}`);
-    }
-  } else {
+  if (!Device.isDevice) {
+    Alert.alert("Error", "Must use physical device for push notifications");
     throw new Error("Must use physical device for push notifications");
+  }
+
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+
+  if (existingStatus !== "granted") {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+
+  if (finalStatus !== "granted") {
+    return new Promise(() => {
+      Alert.alert(
+        "Permission Required",
+        "Push notification permission is required to continue using the app.",
+        [{ text: "OK", onPress: () => registerForPushNotificationsAsync() }]
+      );
+    });
+  }
+
+  const projectId =
+    Constants?.expoConfig?.extra?.eas?.projectId ??
+    Constants?.easConfig?.projectId;
+  if (!projectId) {
+    throw new Error("Project ID not found");
+  }
+
+  try {
+    const pushTokenString = (
+      await Notifications.getExpoPushTokenAsync({ projectId })
+    ).data;
+    console.log(pushTokenString);
+    return pushTokenString;
+  } catch (e) {
+    throw new Error(`${e}`);
   }
 }
